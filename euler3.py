cad411fd28a7a5d0835cd1e215df89989b7f13c5
dblanco940 <dblancoav@gmail.com>
2023-07-18 10:24:00 +0100

    Adding 2ns Euler problem

---
+++ euler3.py
@@ -0,0 +1,50 @@
+
+'''
+Problem 3 
+
+The prime factors of 13195 are 5, 7, 13 and 29
+What is the largest prime factor of the number $600851475143?
+
+'''
+
+def find_factors(number):
+    factors = (i for i in range(1, number) if number % i == 0)
+    return factors 
+
+
+def is_prime_number(anumber):
+    # define whether the number given is prime or not
+    for i in range(2, anumber):
+        if anumber % i == 0:
+            print(i)
+            # Number is not prime
+            return False             
+        else: 
+            return True
+                            
+
+def get_highest_prime_factor(target_number):
+    
+    prime_factors = [factor for factor in find_factors(target_number) if is_prime_number(factor)]
+    
+    return prime_factors
+    
+    
+    # print(f'Factors of {target_number}: {find_factors(target_number)}')
+    
+    # for factor in find_factors(target_number):
+            
+    #     #add the prime numbers to a separate list 
+    #     if is_prime_number(factor):
+    #         prime_factors.append(factor)
+    
+    
+        
+    # print(f'Prime Factors: {prime_factors}')
+    # return max(prime_factors)
+
+# print(get_highest_prime_factor(600851475143))
+print(get_highest_prime_factor(13195))
+
+    
+        
